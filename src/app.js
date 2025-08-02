aecd884fa278deccb5369b49540697d688791222
Zytronium <dastelljes@outlook.com>
2025-08-01 21:20:54 -0500

    Created base Express.js server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+#!/bin/node
+const express = require('express');
+const dotenv = require('dotenv').config({ quiet: true });
+const morgan = require("morgan");
+const routes = require('./routes/index.js');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Log requests to console
+app.use(morgan("dev")); // Or "combined" for more detailed logs
+
+// Allow JSON in requests
+app.use(express.json());
+
+// Serve routes
+app.use(routes);
+
+// Run the server
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
